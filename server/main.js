fbace611ea8320e115ba686b4cd39c65e0a0865c
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2019-12-09 15:13:44 -0700

    Day1done

--- server/main.js
+++ server/main.js
@@ -1,18 +1,26 @@
 import express from "express";
 import bp from "body-parser";
+import cors from "cors";
 import TodoController from "./controllers/TodoController";
 
 let server = express();
 
+server.use(express.static(__dirname + "/../client"));
+
+var whitelist = ["http://localhost:8080"];
+var corsOptions = {
+  origin: function(origin, callback) {
+    var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+    callback(null, originIsWhitelisted);
+  },
+  credentials: true
+};
+server.use(cors(corsOptions));
+
 server.use(bp.urlencoded({ extended: true }));
 server.use(bp.json());
 
-server.get("", (request, response) => {
-  console.log("Hello Request");
-  response.send({ message: "got request", data: { greeting: "hello world" } });
-});
-
-server.use("/todos", new TodoController().router);
+server.use("/api/todos", new TodoController().router);
 
 server.use((error, req, res, next) => {
   res.status(error.status || 400).send({ error: { message: error.message } });
